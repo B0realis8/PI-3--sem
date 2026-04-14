daeff03f180586cdd2998d1d790d998821a97a52
Renan Antonio <renan.amsouza8@gmail.com>
2026-04-14 01:14:10 -0300

    feat(script): implementa no script a tabela instagram

--- scripts/script.sql
+++ scripts/script.sql
@@ -1,19 +1,35 @@
-DROP TABLE IF EXISTS vendas;
+-- Active: 1776135066399@@127.0.0.1@5432@PI 3 SEM
+-- Active: 1776135066399@@127.0.0.1@5432@PI - 3ºsem
+DROP TABLE IF EXISTS vendas CASCADE;
 CREATE TABLE vendas (
+
+id_venda SERIAL PRIMARY KEY,
+data_venda DATE NOT NULL,
+id_orcamento INT REFERENCES orcamento(id_orcamento),
+quantidade INT,
+forma_pgto VARCHAR(20),
+valor_total NUMERIC(20,2),
+entrada NUMERIC(20,2),
+n_parcelas INT,
+valor_parcelas NUMERIC(10,2),
+comissao NUMERIC,
+lucro_total NUMERIC
 
 );
 
-DROP TABLE IF EXISTS cliente;
+DROP TABLE IF EXISTS cliente CASCADE;
 CREATE TABLE cliente (
 
 id_cliente SERIAL PRIMARY KEY ,
 nome VARCHAR(100) NOT NULL,
+sexo CHAR(1) CHECK (sexo in('F','M')),
+data_nascimento DATE,
 cpf CHAR(11) NOT NULL UNIQUE
 
 );
 
 
-DROP TABLE IF EXISTS produto;
+DROP TABLE IF EXISTS produto CASCADE;
 CREATE TABLE produto (
 
 id_produto SERIAL PRIMARY KEY,
@@ -26,7 +42,7 @@ cidade VARCHAR(50)
 );
 
 
-DROP TABLE IF EXISTS servico;
+DROP TABLE IF EXISTS servico CASCADE;
 CREATE TABLE servico (
 
 id_servico SERIAL PRIMARY KEY,
@@ -37,7 +53,7 @@ valor NUMERIC(10,2)
 );
 
 
-DROP TABLE IF EXISTS companhia_aerea;
+DROP TABLE IF EXISTS companhia_aerea CASCADE;
 CREATE TABLE companhia_aerea (
 
 id_companhia SERIAL PRIMARY KEY,
@@ -47,7 +63,7 @@ pais VARCHAR(50)
 );
 
 
-DROP TABLE IF EXISTS voo;
+DROP TABLE IF EXISTS voo CASCADE;
 CREATE TABLE voo (
 
 id_voo SERIAL PRIMARY KEY,
@@ -65,7 +81,7 @@ obs TEXT
 
 );
 
-DROP TABLE IF EXISTS orcamento;
+DROP TABLE IF EXISTS orcamento CASCADE;
 CREATE TABLE orcamento (
 
 id_orcamento SERIAL PRIMARY KEY,
@@ -76,7 +92,7 @@ valor_total NUMERIC (20,2)
 );
 
 
-DROP TABLE IF EXISTS join_servicos_orcamento;
+DROP TABLE IF EXISTS join_servicos_orcamento CASCADE;
 CREATE TABLE join_servicos_orcamento (
 
 PRIMARY KEY(id_servico, id_orcamento),
@@ -86,6 +102,32 @@ quantidade INT
 	
 );
 
-SELECT * FROM orcamento JOIN join_servicos_orcamento USING (id_orcamento)
-SELECT * FROM orcamento JOIN voo USING (id_voo)
-SELECT o.id_orcamento, v.id_voo FROM orcamento o JOIN voo v USING (id_voo)
+
+DROP TABLE IF EXISTS instagram CASCADE;
+CREATE TABLE instagram (
+
+post_id SERIAL PRIMARY KEY,
+account_id INT UNIQUE,
+account_type VARCHAR(20),
+follower_count INT,
+media_type VARCHAR(20),
+content_category VARCHAR(30),
+traffic_source VARCHAR(30),
+has_call_to_action BOOLEAN,
+post_datetime TIMESTAMP,
+post_date DATE,
+post_hour INT,
+day_of_week VARCHAR(20),
+likes INT,
+comments INT,
+shares INT,
+saves INT,
+reach INT,
+impression INT,
+engagement_rate NUMERIC,
+followers_gained INT,
+caption_length INT,
+hashtags_count INT,
+performance_bucket VARCHAR(10)
+
+)
