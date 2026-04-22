542441a56c2ccb8c694905256609a25f3c41175e
Renan Antonio <renan.amsouza8@gmail.com>
2026-04-21 23:39:58 -0300

    feat: modifica o arquivo para conter storage secret e outros parametros

--- src/intercambio_para_todos/__main__.py
+++ src/intercambio_para_todos/__main__.py
@@ -2,10 +2,10 @@ import nicegui as ng
 from nicegui import ui,app
 import psycopg2 as pg
 from modules import db_connection
-from routes import index, page2, produto, instagram
+from routes import page2, produto, instagram,root
 
 app.add_static_files(url_path='src/intercambio_para_todos/assets', local_directory='.')
 
 db_connection.get_data_from_db()
 
-ui.run(native=True)
+ui.run(native=True,storage_secret='my_secret_key', title='Intercâmbio para Todos', favicon='src/intercambio_para_todos/assets/css/images/logo.png')
