5ca14d95cd64482219eaf8d9f0b2f19addfc9ed8
Renan Antonio <renan.amsouza8@gmail.com>
2026-04-21 21:17:26 -0300

    feat(config): adiciona arquivos de config

--- src/intercambio_para_todos/__main__.py
+++ src/intercambio_para_todos/__main__.py
@@ -1,25 +1,11 @@
-import os
-import pandas as pd
+import nicegui as ng
+from nicegui import ui,app
 import psycopg2 as pg
-from psycopg2 import sql
-from dotenv import load_dotenv
+from modules import db_connection
+from routes import index, page2, produto, instagram
 
-load_dotenv()
-db_name = os.getenv("DB_NAME")
-username = os.getenv("USER")
-_port = os.getenv("PORT")
-pswrd = os.getenv("PASSWORD")
-db_host = os.getenv("DB_HOST")
+app.add_static_files(url_path='src/intercambio_para_todos/assets', local_directory='.')
 
-print(db_name)
+db_connection.get_data_from_db()
 
-conn = pg.connect(host=db_host,
-                       dbname=db_name,
-                       user=username,
-                       password=pswrd,
-                       port=_port) 
-
-cur = conn.cursor()
-
-query = sql.SQL("SELECT * FROM {}").format(sql.Identifier("instagram"))
-cur.execute(query)
+ui.run(native=True)
