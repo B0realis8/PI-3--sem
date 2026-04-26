546004f66e0080e9183ee4f9a421d7900c843226
Renan Antonio <renan.amsouza8@gmail.com>
2026-04-26 20:10:06 -0300

    fix: ajusta os caminhos dos arquivos estaticos

--- src/intercambio_para_todos/__main__.py
+++ src/intercambio_para_todos/__main__.py
@@ -3,9 +3,14 @@ from nicegui import ui,app
 import psycopg2 as pg
 from modules import db_connection
 from routes import page2, produto, instagram,root
+import os
+from pathlib import Path
 
-app.add_static_files(url_path='src/intercambio_para_todos/assets', local_directory='.')
+app.add_static_files(url_path='/assets', local_directory=str(Path(__file__).resolve().parent / 'assets'))
 
 db_connection.get_data_from_db()
 
 ui.run(native=True,storage_secret='my_secret_key', title='Intercâmbio para Todos', favicon='src/intercambio_para_todos/assets/css/images/logo.png')
+
+app.add_static_files(url_path='/assets/css/fonts/inter-600.woff2', local_directory=str(Path(__file__).resolve().parent / 'assets/css/fonts'))
+app.add_static_files(url_path='/assets/css/fonts/inter-600.woff', local_directory=str(Path(__file__).resolve().parent / 'assets/css/fonts'))
