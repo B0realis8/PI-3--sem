a0044dd840bbe9665ff178086776cd840c33b4f1
Renan Antonio <renan.amsouza8@gmail.com>
2026-04-26 21:16:52 -0300

    doc: remove arquivos nao utilizados e move arquivo de icones

--- src/intercambio_para_todos/routes/page2.py
+++
@@ -1,6 +0,0 @@
-from nicegui import ui,app,APIRouter
-
-@ui.page('/page2')
-def page2():
-    ui.label('Página 2')
-    ui.button('Voltar para a página inicial', on_click=lambda: ui.navigate.to('/'))
