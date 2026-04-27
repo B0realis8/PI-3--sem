0ed180d12ba1f180e49007ed83289fdb8b5daea9
Renan Antonio <renan.amsouza8@gmail.com>
2026-04-26 21:13:48 -0300

    fix: ajusta o tamanho inicial da janela

--- src/intercambio_para_todos/__main__.py
+++ src/intercambio_para_todos/__main__.py
@@ -10,7 +10,7 @@ app.add_static_files(url_path='/assets', local_directory=str(Path(__file__).reso
 
 db_connection.get_data_from_db()
 
-ui.run(native=True,storage_secret='my_secret_key', title='Intercâmbio para Todos', favicon='src/intercambio_para_todos/assets/css/images/logo.png')
+ui.run(native=True,window_size=(1200, 800),storage_secret='my_secret_key', title='Intercâmbio para Todos', favicon='src/intercambio_para_todos/assets/css/images/logo.png')
 
 app.add_static_files(url_path='/assets/css/fonts/inter-600.woff2', local_directory=str(Path(__file__).resolve().parent / 'assets/css/fonts'))
 app.add_static_files(url_path='/assets/css/fonts/inter-600.woff', local_directory=str(Path(__file__).resolve().parent / 'assets/css/fonts'))
